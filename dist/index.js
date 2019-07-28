67ac70ad0e458e5fffb59f599995448facd66478
Ciara Jhenica Estavillo <estavillocj43@gmail.com>
2019-07-28 22:43:51 +0800

    Initial commit and added files

---
+++ dist/index.js
@@ -0,0 +1,10 @@
+"use strict";
+
+// index.js
+var addNum = function addNum() {
+  var numOne = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : 2;
+  var numbTwo = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 3;
+  return numOne + numbTwo;
+};
+
+addNum();
